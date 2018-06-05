4da8f2ebc8755af101a098948a44ecc173e99245
Yedda <YeddaChuang@gmail.com>
2018-06-05 20:41:11 +0800

    CRUD: finish C & R

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,25 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_url
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    param.require(:task).permit(:name, :due_date, :note)
+  end
 end
